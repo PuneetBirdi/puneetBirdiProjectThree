005ee27110c14dd18af9c3b3f20206e8e3297a88
puneet <birdipuneet@gmail.com>
2020-04-12 09:04:11 -0400

    minimum viable product finished, going to conver the styling to grid to make the responsiveness a little bit better

--- script.js
+++ script.js
@@ -13,52 +13,50 @@ $(document).ready(function(){
         return id;
     }
 
-    //Push the transaction into the array
+    //Push the transaction into the all transactions array and also separate arrays depending on the transaction type
     function updateArray(transaction){
         transactions.push(transaction);
+        updateTotals(transactions);
     }
 
-    //updating totals
-    function updateTotals(transaction){
-        //filter transaction amount depending on its type
-        if(transaction.transactionType === 'income'){
-            totalIncome = totalIncome + transaction.transactionAmt;
-        }else if(transaction.transactionType === 'expense'){
-            totalExpense = totalExpense + transaction.transactionAmt;
-        }
+    //update total summary values
+    function updateTotals(array){
+        totalIncome = 0;
+        totalExpense = 0;
+        array.forEach(element => {
+            if(element.transactionType === "income"){
+                totalIncome = totalIncome + element.transactionAmt;
+            }else if(element.transactionType === "expense"){
+                totalExpense = totalExpense + element.transactionAmt;
+            }
+        });
         netCashFlow = totalIncome - totalExpense;
-
-        return netCashFlow, totalIncome, totalExpense;
     }
-
     //format into money - this was taken from a stackoverflow comment
     function formatMoney(number){
         return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
       }
 
-    //update DOM function
-    function updateDOM(transaction, totalIncome, totalExpense, netCashflow){
+    //update summary values in DOM
+    function updateDOMSummary(totalIncome, totalExpense, netCashflow){
 
         //update totals
         $("#totalIncome").html(formatMoney(totalIncome));
         $("#netCashflow").html(formatMoney(netCashFlow)); 
         $("#totalExpense").html(formatMoney(totalExpense));
-
-        //append list
+        }
+    //update list in DOM
+    function updateDOMList(transaction){
         $("#transactionList").prepend(
-            `<li class=${transaction.transactionType} id=${transaction.transactionID}>
-            <div class='listBtns'>
-                <button class='deleteBtn btn')>
-                Remove
-                </button>
-
-                <button class='editBtn btn'>
-                Edit
-                </button>
-            </div>
-            <div class='listDesc'>${transaction.transactionDesc}</div>
-            <div class='listAmt'>${formatMoney(transaction.transactionAmt)}</div>
-            </li>`)
+            `<tr id=${transaction.transactionID} class= "${transaction.transactionType}">
+                <td class = 'transactionBtn'>
+                    <button id="deleteBtn" data-transactionID= ${transaction.transactionID}><i class="fas fa-trash-alt fa-lg")"></i></button>
+                    <button id="editBtn" data-transactionID= ${transaction.transactionID}<i class="fas fa-sliders-h fa-lg"></i>
+                </td>
+                <td class='description'>${transaction.transactionDesc}</td>
+                <td class="amount">${formatMoney(transaction.transactionAmt)}</td>
+            </tr>`
+          )
     }
 
     //adjust the form, used for both clearing and editing
@@ -68,6 +66,20 @@ $(document).ready(function(){
         $("#transactionDesc").val(transactionDesc);
         $("#transactionAmt").val(transactionAmt);
     }
+
+    //event listener for delete transaction
+    $(document).on("click", '#deleteBtn', function(){
+        deleteID = $(this).attr('data-transactionID');
+        //remove transaction from DOM
+        $(`#${deleteID}`).remove();
+        //filter out transaction from array
+        transactions = transactions.filter(function(object){
+            return (object.transactionID != deleteID)
+        })
+        //update total values
+        updateTotals(transactions);
+        updateDOMSummary(totalIncome, totalExpense, netCashFlow);
+    });
 
     //event listener for when transaction is submitted
     $("form").submit(function(){
@@ -80,37 +92,22 @@ $(document).ready(function(){
                 transactionDesc: $("#transactionDesc").val(),
                 transactionAmt: parseFloat($("#transactionAmt").val())
                 }
-            
-                console.log(transaction);
-            
         //if variables satisfy requirements, run the next functions, otherwise throw an alert error
             if(
             typeof transaction.transactionType !== 'undefined' && 
             transaction.transactionDesc.trim() !== "" &&
             transaction.transactionAmt > 0
             ){
-            //function to update totals
-            updateTotals(transaction);
-            //function to update DOM with totals
-            updateDOM(transaction, totalIncome, totalExpense, netCashFlow);
             //call function to update the array;
             updateArray(transaction);
+            //function to update DOM with totals
+            updateDOMSummary(totalIncome, totalExpense, netCashFlow);
+            //update DOM List
+            updateDOMList(transaction);
             //reset form
             adjustForm("", "", "");
             }else{
                 alert("Please fill out all of the required inputs.")
             }
         });
-    
-    //event listener for delete transaction
-    $("form").submit(function(){
-        deleteTransaction();
-    }
-    function deleteTransaction(transactionID){
-        //remove the transaction from the DOM
-        $(`${transactionID}`).remove();
-        //remove the transaction from the array
-
-        //updateDOM
-    }
 });
